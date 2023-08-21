72e151869320b65b13aa4a3632b2e2c3ed5620c7
Phil <supersmashphil@gmail.com>
2023-08-21 08:45:35 -0600

    corners for region type

--- Source/Vehicles/Pathing/RegionGrid/VehicleRegionTypeUtility.cs
+++ Source/Vehicles/Pathing/RegionGrid/VehicleRegionTypeUtility.cs
@@ -52,6 +52,7 @@ namespace Vehicles
 			return RegionType.ImpassableFreeAirExchange;
 		}
 
+		//TODO - Account for non-uniform combinations (eg. Y shape)
 		private static bool VerifyCardinalCellSpace(IntVec3 cell, VehicleMapping mapping, VehicleDef vehicleDef)
 		{
 			if (vehicleDef.Size.x % 2 == 0 || vehicleDef.Size.z % 2 == 0)
@@ -61,11 +62,23 @@ namespace Vehicles
 				IntVec3 east = new IntVec3(cell.x + padding, 0, cell.z);
 				IntVec3 south = new IntVec3(cell.x, 0, cell.z - padding);
 				IntVec3 west = new IntVec3(cell.x - padding, 0, cell.z);
+				IntVec3 northEast = new IntVec3(cell.x + padding, 0, cell.z + padding);
+				IntVec3 southEast = new IntVec3(cell.x + padding, 0, cell.z - padding);
+				IntVec3 southWest = new IntVec3(cell.x - padding, 0, cell.z - padding);
+				IntVec3 northWest = new IntVec3(cell.x - padding, 0, cell.z + padding);
 				if (!vehicleDef.WidthStandable(mapping.map, north) && !vehicleDef.WidthStandable(mapping.map, south))
 				{
 					return false;
 				}
 				else if (!vehicleDef.WidthStandable(mapping.map, east) && !vehicleDef.WidthStandable(mapping.map, west))
+				{
+					return false;
+				}
+				else if (!vehicleDef.WidthStandable(mapping.map, northEast) && !vehicleDef.WidthStandable(mapping.map, southWest))
+				{
+					return false;
+				}
+				else if (!vehicleDef.WidthStandable(mapping.map, southEast) && !vehicleDef.WidthStandable(mapping.map, northWest))
 				{
 					return false;
 				}
