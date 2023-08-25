804a9a4d251c444169e1813863adb265e80a99e0
Phil <supersmashphil@gmail.com>
2023-08-24 21:40:08 -0600

    added internal hitbox prioritization

--- Source/Vehicles/Components/Vehicles/Health/VehicleStatHandler.cs
+++ Source/Vehicles/Components/Vehicles/Health/VehicleStatHandler.cs
@@ -321,22 +321,32 @@ namespace Vehicles
 						report?.AppendLine($"components=({string.Join(",", components.Select(c => c.props.label))})");
 						report?.AppendLine($"hitDepth = {hitDepth}");
 						//If no components at hit cell, fallthrough to internal
-						if (!components.Where(comp => comp.props.depth == hitDepth && comp.HealthPercent > 0).TryRandomElementByWeight((component) => component.props.hitWeight, out component))
+						var externalComponentsAtHitDepth = components.Where(comp => comp.props.depth == hitDepth && comp.HealthPercent > 0);
+						report?.AppendLine($"components at hitDepth {hitDepth}: ({string.Join(",", externalComponentsAtHitDepth.Select(comp => comp.props.label))})");
+						if (!externalComponentsAtHitDepth.TryRandomElementByWeight((component) => component.props.hitWeight, out component))
 						{
 							report?.AppendLine($"No components found. Hitting internal parts.");
 							hitDepth = VehicleComponent.VehiclePartDepth.Internal;
-							//If depth = internal then pick random internal component even if it does not have a hitbox
-							component = this.components.Where(comp => comp.props.depth == hitDepth && comp.HealthPercent > 0).RandomElementByWeightWithFallback((component) => component.props.hitWeight);
-							//If no internal components, pick random component w/ health
-							component ??= this.components.Where(comp => comp.HealthPercent > 0).RandomElementByWeightWithFallback((component) => component.props.hitWeight);
-							if (component is null)
+							var internalComponentsAtHitDepth = components.Where(comp => comp.props.depth == hitDepth && comp.HealthPercent > 0);
+							if (!internalComponentsAtHitDepth.TryRandomElementByWeight((component) => component.props.hitWeight, out component))
 							{
-								return;
+								//If depth = internal then pick random internal component even if it does not have a hitbox
+								component = this.components.Where(comp => comp.props.depth == hitDepth && comp.HealthPercent > 0).RandomElementByWeightWithFallback((component) => component.props.hitWeight);
+								//If no internal components, pick random component w/ health
+								component ??= this.components.Where(comp => comp.HealthPercent > 0).RandomElementByWeightWithFallback((component) => component.props.hitWeight);
+								if (component is null)
+								{
+									return;
+								}
+							}
+							else
+							{
+								report?.AppendLine($"Found Internal Component {component.props.label} at {hitCell}");
 							}
 						}
 						else
 						{
-							report?.AppendLine($"Found {component} at {hitCell}");
+							report?.AppendLine($"Found External Component {component.props.label} at {hitCell}");
 						}
 					}
 					else
