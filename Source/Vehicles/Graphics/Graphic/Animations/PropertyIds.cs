f068ed145b44f240a239a2714c1f6ba3281db356
Phil <supersmashphil@gmail.com>
2024-10-19 18:42:47 -0600

    animation work

--- Source/Vehicles/Graphics/Graphic/Animations/PropertyIds.cs
+++ Source/Vehicles/Graphics/Graphic/Animations/PropertyIds.cs
@@ -8,6 +8,13 @@ namespace Vehicles
 {
 	public static class PropertyIds
 	{
+		public static int Moving = "Moving".GetHashCode();
+		public static int Dead = "Dead".GetHashCode();
+		public static int IgnitionOn = "IgnitionOn".GetHashCode();
+
+		public static int PropellerSpinUp = "PropellerSpinUp".GetHashCode();
+		public static int Launch = "Launch".GetHashCode();
+
 		public static int Explode = "Explode".GetHashCode();
 	}
 }
