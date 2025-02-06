b80b7ab621d4b155a3e35c1174cc9677b32ecab3
Phil <supersmashphil@gmail.com>
2025-02-05 23:01:33 -0700

    added files, example animations for testing

---
+++ Source/Vehicles/AI/JobGivers/NPC/JobGiver_ExitMapRandom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Vehicles
+{
+	public class JobGiver_ExitMapRandom : JobGiver_ExitMap
+	{
+		protected override bool TryFindGoodExitDest(VehiclePawn vehicle, out IntVec3 cell)
+		{
+			// TODO - Add sapper capabilities for a vehicle to blast its way out
+			return CellFinderExtended.TryFindRandomExitSpot(vehicle, out cell);
+		}
+	}
+}
---
+++ Source/Vehicles/Harmony/UnitTesting/UnitTest_MapDeinit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using RimWorld.Planet;
+using SmashTools;
+using SmashTools.Debugging;
+using SmashTools.Performance;
+using Verse;
+using Verse.Profile;
+
+namespace Vehicles.Testing
+{
+	internal class UnitTest_MapDeinit : UnitTest
+	{
+		public override TestType ExecuteOn => TestType.GameLoaded;
+
+		public override ExecutionPriority Priority => ExecutionPriority.Last;
+
+		public override string Name => "Map Deinit";
+
+		public override IEnumerable<UTResult> Execute()
+		{
+			Assert.IsNotNull(Current.Game);
+			Assert.IsNotNull(Find.CurrentMap);
+
+			VehicleMapping mapping = Find.CurrentMap.GetCachedMapComponent<VehicleMapping>();
+			Assert.IsNotNull(mapping);
+
+			// Testing that all threads can be terminated and the caller will wait until they finish
+			ThreadManager.ReleaseThreadsAndClearCache();
+			yield return UTResult.For("Threads Terminated", ThreadManager.AllThreadsTerminated);
+		}
+	}
+}
