316b1cd9d65ac734eb5efde72a79334851b7d6a3
Phil <supersmashphil@gmail.com>
2025-08-02 21:05:37 -0600

    VF-179: Removed 'haul thing' gizmo from everything expect haulable resources.

--- Source/Vehicles/Utility/Extensions/Ext_Thing.cs
+++ Source/Vehicles/Utility/Extensions/Ext_Thing.cs
@@ -1,101 +1,98 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using RimWorld;
 using SmashTools;
 using Verse;
 using Verse.AI;
 
-namespace Vehicles
+namespace Vehicles;
+
+public static class Ext_Thing
 {
-  public static class Ext_Thing
+  public static bool CanBeTransferredToVehiclesCargo(this Thing thing)
   {
-    public static bool CanBeTransferredToVehiclesCargo(this Thing thing)
+    return thing.def.EverHaulable;
+  }
+
+  public static IEnumerable<VehiclePawn> GetVehiclesToBeTransferredTo(this Thing thing)
+  {
+    Map map = thing.MapHeld;
+
+    if (map == null)
     {
-      return thing.def.EverHaulable || (thing is Pawn { Faction.IsPlayer: true }
-                                          or Pawn { Downed: true } and not VehiclePawn);
+      yield break;
     }
 
-    public static IEnumerable<VehiclePawn> GetVehiclesToBeTransferredTo(this Thing thing)
+    IEnumerable<VehiclePawn> vehicles = map.GetCachedMapComponent<VehicleReservationManager>()
+     .VehicleListers(ReservationType.LoadVehicle);
+
+    foreach (VehiclePawn vehicle in vehicles)
     {
-      Map map = thing.MapHeld;
-
-      if (map == null)
+      if (vehicle.cargoToLoad?.FindTransferableFor(thing) != null)
       {
-        yield break;
-      }
-
-      IEnumerable<VehiclePawn> vehicles = map.GetCachedMapComponent<VehicleReservationManager>()
-       .VehicleListers(ReservationType.LoadVehicle);
-
-      foreach (VehiclePawn vehicle in vehicles)
-      {
-        if (vehicle.cargoToLoad?.FindTransferableFor(thing) != null)
-        {
-          yield return vehicle;
-        }
+        yield return vehicle;
       }
     }
+  }
 
-    public static bool IsOrderedToBeTransferredToAnyVehicle(this Thing thing)
+  public static bool IsOrderedToBeTransferredToAnyVehicle(this Thing thing)
+  {
+    return thing.GetVehiclesToBeTransferredTo().Any();
+  }
+
+  public static void TransferToVehicle(this IEnumerable<Thing> things, VehiclePawn vehicle)
+  {
+    foreach (Thing thing in things)
     {
-      return thing.GetVehiclesToBeTransferredTo().Any();
+      thing.CancelTransferToAnyOtherVehicle(vehicle);
+      thing.TransferToVehicle(vehicle);
     }
 
-    public static void TransferToVehicle(this IEnumerable<Thing> things, VehiclePawn vehicle)
+    vehicle.MapHeld?.GetCachedMapComponent<VehicleReservationManager>()
+     .RegisterLister(vehicle, ReservationType.LoadVehicle);
+  }
+
+  public static void TransferToVehicle(this Thing thing, VehiclePawn vehicle)
+  {
+    (vehicle.cargoToLoad ??= []).AddThing(thing);
+  }
+
+  public static void CancelTransferToVehicle(this Thing thing, VehiclePawn vehicle)
+  {
+    if (vehicle.cargoToLoad?.RemoveThing(thing) == true)
     {
-      foreach (Thing thing in things)
+      thing.CancelRelatedJob(JobDefOf_Vehicles.LoadVehicle, JobCondition.QueuedNoLongerValid);
+    }
+  }
+
+  public static void CancelTransferToAnyOtherVehicle(this Thing thing, VehiclePawn vehicle)
+  {
+    foreach (VehiclePawn otherVehicle in thing.GetVehiclesToBeTransferredTo())
+    {
+      if (otherVehicle != vehicle)
       {
-        thing.CancelTransferToAnyOtherVehicle(vehicle);
-        thing.TransferToVehicle(vehicle);
-      }
-
-      vehicle.MapHeld?.GetCachedMapComponent<VehicleReservationManager>()
-       .RegisterLister(vehicle, ReservationType.LoadVehicle);
-    }
-
-    public static void TransferToVehicle(this Thing thing, VehiclePawn vehicle)
-    {
-      (vehicle.cargoToLoad ??= []).AddThing(thing);
-    }
-
-    public static void CancelTransferToVehicle(this Thing thing, VehiclePawn vehicle)
-    {
-      if (vehicle.cargoToLoad?.RemoveThing(thing) == true)
-      {
-        thing.CancelRelatedJob(JobDefOf_Vehicles.LoadVehicle, JobCondition.QueuedNoLongerValid);
-      }
-    }
-
-    public static void CancelTransferToAnyOtherVehicle(this Thing thing, VehiclePawn vehicle)
-    {
-      foreach (VehiclePawn otherVehicle in thing.GetVehiclesToBeTransferredTo())
-      {
-        if (otherVehicle != vehicle)
-        {
-          thing.CancelTransferToVehicle(otherVehicle);
-        }
-      }
-    }
-
-    public static void CancelTransferToAnyVehicle(this Thing thing)
-    {
-      foreach (VehiclePawn vehicle in thing.GetVehiclesToBeTransferredTo())
-      {
-        thing.CancelTransferToVehicle(vehicle);
+        thing.CancelTransferToVehicle(otherVehicle);
       }
     }
+  }
 
-    public static void CancelRelatedJob(this Thing thing, JobDef jobType,
-      JobCondition cancelCondition)
+  public static void CancelTransferToAnyVehicle(this Thing thing)
+  {
+    foreach (VehiclePawn vehicle in thing.GetVehiclesToBeTransferredTo())
     {
-      ReservationManager.Reservation reservation =
-        thing.MapHeld?.reservationManager.ReservationsReadOnly.FirstOrFallback(res =>
-          res?.Target.Thing == thing);
+      thing.CancelTransferToVehicle(vehicle);
+    }
+  }
 
-      if (reservation?.Job.def == jobType)
-      {
-        reservation.Job.GetCachedDriver(reservation.Claimant).EndJobWith(cancelCondition);
-      }
+  public static void CancelRelatedJob(this Thing thing, JobDef jobType,
+    JobCondition cancelCondition)
+  {
+    ReservationManager.Reservation reservation =
+      thing.MapHeld?.reservationManager.ReservationsReadOnly.FirstOrFallback(res =>
+        res?.Target.Thing == thing);
+
+    if (reservation?.Job.def == jobType)
+    {
+      reservation.Job.GetCachedDriver(reservation.Claimant).EndJobWith(cancelCondition);
     }
   }
 }
