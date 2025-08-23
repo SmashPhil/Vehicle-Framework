f92fe2618cc0ef92de33f0c000f6bf4a82d00479
Phil <supersmashphil@gmail.com>
2025-08-22 18:04:08 -0600

    VF-204: Fixed compatibility issue with Hospitality destructive patching over gathering method that prevented vehicles from joining parties.

--- Source/Vehicles/Harmony/Patches/Patch_LordAi.cs
+++ Source/Vehicles/Harmony/Patches/Patch_LordAi.cs
@@ -7,24 +7,26 @@ namespace Vehicles;
 
 internal class Patch_LordAi : IPatchCategory
 {
-  PatchSequence IPatchCategory.PatchAt => PatchSequence.Async;
+	PatchSequence IPatchCategory.PatchAt => PatchSequence.Async;
 
-  void IPatchCategory.PatchMethods()
-  {
-    HarmonyPatcher.Patch(
-      original: AccessTools.Method(typeof(GatheringsUtility),
-        nameof(GatheringsUtility.ShouldGuestKeepAttendingGathering)),
-      prefix: new HarmonyMethod(typeof(Patch_LordAi),
-        nameof(VehiclesDontParty)));
-  }
+	void IPatchCategory.PatchMethods()
+	{
+		// NOTE - Hospitality patches over this completely with the assumption that only androids would skip food needs checks.
+		// Setting this to higher priority will at least let vehicles exit early before Hospitality's destructive patch.
+		HarmonyPatcher.Patch(
+			original: AccessTools.Method(typeof(GatheringsUtility),
+				nameof(GatheringsUtility.ShouldGuestKeepAttendingGathering)),
+			prefix: new HarmonyMethod(AccessTools.Method(typeof(Patch_LordAi), nameof(VehiclesDontParty)),
+				priority: Priority.First));
+	}
 
-  public static bool VehiclesDontParty(Pawn p, ref bool __result)
-  {
-    if (p is VehiclePawn)
-    {
-      __result = false;
-      return false;
-    }
-    return true;
-  }
+	public static bool VehiclesDontParty(Pawn p, ref bool __result)
+	{
+		if (p is VehiclePawn)
+		{
+			__result = false;
+			return false;
+		}
+		return true;
+	}
 }
